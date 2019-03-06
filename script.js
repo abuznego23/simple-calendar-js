105bd5a4cf77cdb763834aafb0e8d60c49c8527e
abuznego <46837889+abuznego23@users.noreply.github.com>
2019-03-06 00:45:32 -0800

    last minute changes

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 var today = new Date();
 var current_month = today.getMonth();
 var current_year = today.getFullYear();
-window.onload = function(){
-	displayCalendar(current_month, current_year);
-}
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// Cached elements
+var calendar_title = document.getElementById("current-month-year");
+var tbody = document.getElementById("calendar-body");
+
+displayCalendar(current_month, current_year);
+
 function displayCalendar(month, year){ 
-	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	document.getElementById("current-month-year").innerHTML = months[month] + " " + year;
+	calendar_title.innerHTML = months[month] + " " + year;
 	var first_day = new Date(year, month, 1);
 	var weekday_start = first_day.getDay();
-	var tbody = document.getElementById("calendar-body");
 	tbody.innerHTML = "" ;
 	date = 1;
 	for(var row = 0; row < 6; row++){
 		var tr = document.createElement('tr');
 		for(var cell = 0; cell < 7; cell++){
-			if(row==0 && cell< weekday_start){
-				var td = document.createElement('td');
+			var td = document.createElement('td');
+			if(row===0 && cell< weekday_start){
 				td.appendChild(document.createTextNode(""));
 			}else if(date > getDaysInMonth(month, year)){
 				break;
 			}else{
-				var td = document.createElement('td');
 				var full_date = new Date(year, month, date);
 				if(date == today.getDate() && year == today.getFullYear() && month == today.getMonth()){
 					td.className += "today ";
@@ -42,13 +44,13 @@ function getDaysInMonth(month, year){
 }
 function prevMonth(){
 	current_year = (current_month === 0) ? current_year - 1 : current_year;
-    current_month = (current_month === 0) ? 11 : current_month - 1;
-    displayCalendar(current_month, current_year);
+	current_month = (current_month === 0) ? 11 : current_month - 1;
+	displayCalendar(current_month, current_year);
 }
 function nextMonth(){
 	current_year = (current_month === 11) ? current_year + 1 : current_year;
-    current_month = (current_month + 1) % 12;
-    displayCalendar(current_month, current_year);
+	current_month = (current_month + 1) % 12;
+	displayCalendar(current_month, current_year);
 }
 document.addEventListener('click', function (event) {
 	var span = document.getElementsByClassName("close")[0];
@@ -58,9 +60,9 @@ document.addEventListener('click', function (event) {
 	if(!event.target.matches('td.clickable')) return;
 	event.preventDefault();
 	day_clicked = Number(event.target.innerHTML);
-	modal_paragraph.textContent = "You clicked on " + (current_month+1) + "/" + day_clicked + "/" + current_year + "!"
+	modal_paragraph.textContent = "You clicked on " + (current_month+1) + "/" + day_clicked + "/" + current_year + "!";
 	modal.style.display = "block";
 	span.onclick = function() {
-	  modal.style.display = "none";
-	}
+		modal.style.display = "none";
+	};
 }, false);
